566ab9d05598735bd0bd2903f5c267c48f327a16
Sergey Bely <sergiuszbelyj@gmail.com>
2019-03-07 09:55:46 +0100

    24: Override

---
+++ src/main/java/Override.java
@@ -0,0 +1,24 @@
+class   ParentOv {
+   ParentOv method() throws Exception{
+        return new ParentOv();
+    }
+    static class Child extends ParentOv {
+        @java.lang.Override
+       Child method() throws RuntimeException {
+            return new Child();
+        }
+    }
+
+
+public static class Override {
+    public static void main(String[] args) throws Exception {
+        ParentOv parent = new ParentOv();
+        ParentOv child = new Child();
+        System.out.println(parent.method());
+        System.out.println(child.method());
+    }
+}
+
+
+
+}
