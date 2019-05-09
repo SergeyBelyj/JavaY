c0a7203537f891112e303c342aaf7c47e0f13ca5
Sergey Bely <sergiuszbelyj@gmail.com>
2019-05-09 16:06:18 +0200

    Distinct

--- src/main/java/Hibernate/WhatIsHibernate.java
+++ src/main/java/Hibernate/WhatIsHibernate.java
@@ -1,12 +1,15 @@
 package Hibernate;
 
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class WhatIsHibernate extends Thread{
     public static void main(String[] args) {
@@ -17,13 +20,14 @@ public class WhatIsHibernate extends Thread{
           session.beginTransaction();
 //          Car car = new Car("12");
 //          session.persist(car);
-          Query query = session.createQuery("from Car  where number = :number2 or id = :myId");
-          query.setParameter("number2", "321");
-          query.setParameter("myId",3);
+          Query query = session.createQuery("select  distinct c.number from Car c");
+//          query.setParameter("number2", "321");
+//          query.setParameter("myId",3);
+          List<Car> list = query.list();
+          System.out.println(list.size());
 
-          Car car = (Car) query.uniqueResult();
-          System.out.println(car.getId());
-//          List<Car> list = query.list();
+//          Car car = (Car) query.uniqueResult();
+//          System.out.println(car.getId());
           session.getTransaction().commit();
 
       }
